916ea7cdbc75b617f18c928017226feb1f180ecd
evileyelivelx <53938299+evileyelivelx@users.noreply.github.com>
2020-06-04 16:24:11 +1200

    Create add_remover_users.py

---
+++ add_remover_users.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import users_mgt as um
+
+
+# In[2]:
+
+
+um.create_user_table()
+
+
+# In[4]:
+
+
+um.add_user('test2','test2','test2@test.com')
+
+
+# In[7]:
+
+
+um.show_users()
+
+
+# In[6]:
+
+
+um.del_user('test')
+
+
+# In[ ]:
+
+
+
+
